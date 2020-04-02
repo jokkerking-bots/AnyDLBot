7bf87f428d825269f6534e0e63948ff9dab148a0
jokkerking-bots <63076000+jokkerking-bots@users.noreply.github.com>
2020-04-03 03:09:55 +0500

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,10 +1,17 @@
 class Translation(object):
-    START_TEXT = """This is an @AnyDLBot clone created using the Open Source code available.
-Source Code: https://github.com/SpEcHiDe/AnyDLBot
-© @AnyDLBot"""
+    START_TEXT = """🙋Hello,
+This is a Telegram URL Upload Bot!
+
+<b>☺Please send me any direct download URL Link, i can upload to telegram as File/Video
+
+I also I have a lot of functions📥</b>
+
+/help for more details..
+
+Support : @JokkerKing"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "This is an @AnyDLBot clone created using the Open Source <a href='https://github.com/SpEcHiDe/AnyDLBot'>code</a>. \nStart @AnyDLBot to know Paid Plans!"
+    UPGRADE_TEXT = "😅<b>This is Free Service. No Upgrade Plans </b>😜  /help for Details"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
@@ -26,27 +33,28 @@ URL | filename | username | password"""
     NO_VOID_FORMAT_FOUND = "no-one gonna help you\n<b>YouTubeDL</b> said: {}"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     CURENT_PLAN_DETAILS = """Current plan details
---------
+FULL FREE😅
 Telegram ID: <code>{}</code>
-Plan name: <a href='https://t.me/SpEcHlDe/599'>{}</a>
-Expires on: {}"""
+Plan name: <a href='https://t.me/https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ'>Free User</a>
+Expires on: Until My death
+Contact: @JokkerKing"""
     HELP_USER = """There are multiple things I can do:
-👉 <a href="https://t.me/SpEcHlDe/608">All Supported Video / File Formats, with custom file name and custom thumbnail support</a>
-👉 <a href="https://t.me/SpEcHlDe/609">Upload as file from any HTTP link, with custom thumbnail support</a>
-👉 <a href="https://telegram.dog/AnyDLBot">Convert To Streamable Video, any Telegram file</a>
-👉 <a href="https://telegram.dog/AnyDLBot">Convert To Telegram Audio, the media sent as Telegram Documents</a>
-👉 <a href="https://t.me/SpEcHlDe/610">ReName Telegram files, with custom thumbnail support</a>
-👉 <a href="https://t.me/SpEcHlDe/625">Generate Custom Thumbnail by sending two photos in a Media Album</a>
-👉 <a href="https://t.me/SpEcHlDe/653">Trim large videos</a>, and <a href="https://t.me/SpEcHlDe/652">Take Screenshots</a> of Telegram media files.
-👉 <a href="https://t.me/SpEcHlDe/657">Extract compressed Telegram media</a>
-👉 <a href="https://t.me/SpEcHlDe/660">Get a Telegram sticker as a Telegram downloadable media</a>
+👉 <a href="https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ">All Supported Video / File Formats, with custom file name and custom thumbnail support</a>
+👉 <a href="https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ">Upload as file from any HTTP link, with custom thumbnail support</a>
+👉 <a href="https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ">Convert To Streamable Video, any Telegram file</a>
+👉 <a href="https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ">Convert To Telegram Audio, the media sent as Telegram Documents</a>
+👉 <a href="https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ">ReName Telegram files, with custom thumbnail support</a>
+👉 <a href="https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ">Generate Custom Thumbnail by sending two photos in a Media Album</a>
+👉 <a href="https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ">Trim large videos</a>, and <a href="https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ">Take Screenshots</a> of Telegram media files.
+👉 <a href="https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ">Extract compressed Telegram media</a>
+👉 <a href="https://www.youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ">Get a Telegram sticker as a Telegram downloadable media</a>
 --------
 Send /me to know current plan details"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
-    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @AnyDLBot"
+    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @AllOneTgBot"
     FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS [HH:MM:SS]"""
     FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
     FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
@@ -57,11 +65,11 @@ Send /me to know current plan details"""
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
     INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
-    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/FFMpegRoBot"
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/any_postbot"
     EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
     EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
     UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
-    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/JokkerKing'>@JokkerKing</a>"
     EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
 You can use /rename command after receiving file to rename it with custom thumbnail support."""
     CANCEL_STR = "Process Cancelled"
